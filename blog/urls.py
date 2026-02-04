e8dadfa4e5498d8a71e24b7234abe27a5b615e5d
Asmar Ali <asmarali43210@gmail.com>
2026-02-04 22:12:35 +0500

    Added views to create/edit blblog inside the site

--- blog/urls.py
+++ blog/urls.py
@@ -8,5 +8,7 @@ from django.shortcuts import render
 urlpatterns = [
     path('', views.post_list, name='p_l'),
     path('hello/', views.hello, name='hello'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail')
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
